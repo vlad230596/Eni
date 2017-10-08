c45b2a4c5a61890f13dd1674345dddc12233d9f0
Mohnachov Vladislav <vlad230596@mail.ru>
2017-10-09 00:37:45 +0300

    Added first module for Gpio.

---
+++ ConfigTemplate.h
@@ -0,0 +1,21 @@
+/*
+ * ConfigTemplate.h
+ *
+ *  Created on: 9 окт. 2017 г.
+ *      Author: Vladislav
+ */
+
+#ifndef CONFIGTEMPLATE_H_
+#define CONFIGTEMPLATE_H_
+
+//#define CMSIS_INCLUDE_FILE	"stm32f1xx.h"
+//#define HAL_INCLUDE_FILE	"stm32f1xx_hal.h"
+
+//Gpio module
+//#define ENI_GPIO
+
+
+
+
+
+#endif /* CONFIGTEMPLATE_H_ */
