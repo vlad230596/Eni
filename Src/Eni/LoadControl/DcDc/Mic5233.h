a9db2f309beb45d524a547470d408e5f89dfcd6f
vgtutf081 <vgtutf081@gmail.com>
2024-03-27 12:03:44 +0300

    add Tpl810

--- Src/Eni/LoadControl/DcDc/Mic5233.h
+++ Src/Eni/LoadControl/DcDc/Mic5233.h
@@ -29,4 +29,30 @@ namespace Eni::LoadControl {
 	private:
 	};
 
+	class Tpl810 : public Aoz1284, public AdjustableControl {
+	public:
+		Tpl810(const GpioPin& enable, const GpioPin& feedbackPwm, Pwm& pwm) :
+			Aoz1284(enable),
+			AdjustableControl(pwm, 1.2f, 100.f, 12.f, 30.f, 30.f)
+		{
+			pwm.setPins({feedbackPwm.nativePinId});
+		}
+
+		bool setMode(Mode mode) {
+			Aoz1284::setMode(mode);
+			return true;
+		}
+
+		bool enableAdjusting(bool enabled) {
+			if(enabled) {
+				return _pwm.start();
+			} else {
+				_pwm.stop();
+				return true;
+			}
+		}
+
+	private:
+	};
+
 }
